763f472f5ce97eeff8308803f630d4e03b91c715
Benjamin Liu <Benjamin Liu>
2016-11-29 23:01:40 -0800

    moved some code to model

--- src/StoreModel.java
+++ src/StoreModel.java
@@ -1,4 +1,177 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class StoreModel {
+	
+	private Connection conn;
+	private Statement stmt;
 
+	public StoreModel(String USER, String PASS){
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver"); 
+	    } catch (ClassNotFoundException e) {
+	        System.out.println(e.getMessage());
+	    }
+		
+		String URL = "jdbc:mysql://localhost:3306/";
+		
+		try {
+			
+			//Open Connection
+			conn = DriverManager.getConnection(URL, USER, PASS);
+			
+		    stmt = conn.createStatement();
+		    
+			//Drop FruitStore DB 
+		    String sql = "DROP DATABASE FRUITSTORE";
+		    stmt.executeUpdate(sql);
+		    
+		    //Create FruitStore DB
+		    sql = "CREATE DATABASE FRUITSTORE";
+		    stmt.executeUpdate(sql);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.print(e.getMessage());
+		}
+		
+	}
+	
+	public void createTables(){
+		
+		/*
+		 * User(uID, username, password, email, isEmployee)
+		   Fruit (fID, name, price)
+		   Inventory(iID, fID, timeStamp, expirationDate, amount)
+		   Order (oID, uID, timeStamp,  totalPrice, updatedAt)
+		   OrderItem (oiID, oID, fID, amount)
+		   Archive(oID, uID, timeStamp, totalPrice)
+		 * 
+		 * */
+		
+		try {
+		    
+			//Drop FruitStore DB 
+		    String sql = "";
+		    stmt.executeUpdate(sql);
+		    
+		    //Create FruitStore DB
+		    sql = "CREATE DATABASE FRUITSTORE";
+		    stmt.executeUpdate(sql);
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.print(e.getMessage());
+		}
+		
+	}
+	
+	/*
+	 * */
+	public void createUser(String username, String password, String email){
+		//PreparedStatement pStatement = conn.prepareStatement("INSERT INTO User VALUES(?????)");
+	   
+	    String query = "INSERT INTO User(username,password,email) VALUES('" + username +"'," + password + ",'" + email +"')";
+	    
+	    Statement stmtDrop;
+		try {
+			stmtDrop = conn.createStatement();
+			stmtDrop.execute(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	      
+	}
+	
+	/*
+	 * 
+	 * */
+	public void editUser(){
+		
+	}
+	
+	/*
+	 * */
+	public void getUser(){
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	public void makeOrder(){
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	public void editOrder(){
+		
+	}
+	
+	/*
+	 * */
+	public void getOrders(){
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	public void addFruit(){
+		
+	}
+
+	/*
+	 * */
+	public void getListOfFruit(){
+
+	}
+	
+	/*
+	 * */
+	public void getInventory(){
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	public void removeExpired(){
+		
+	}
+	
+	/*
+	 * 
+	 * */
+	public void addReview(){
+		
+	}
+	
+	/*
+	 * */
+	public void editReview(){
+		
+	}
+	
+	/*
+	 * */
+	public void getReview(){
+		
+	}
+	
+	/*
+	 * */
+	public void getStat(){
+		
+	}
+	
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,13 +2,19 @@ import java.awt.Dimension;
 import java.sql.SQLException;
 
 public class Tester {
+	
+	//Credentials for Database
+	public static final String USER = "root";
+	public static final String PASSWORD = "";
+	
+	
 	public static void main(String args[]){
 		
 		String[] listOfFruit = { "pear", "apple" };
 		
 		StoreGUI fruitStore = new StoreGUI(listOfFruit);
-			Store test = new Store("root","!@)59380");
-			test.createUser();
+			StoreModel test = new StoreModel(USER, PASSWORD);
+			//test.createUser();
 		
 		
 	}
