3ef0496d586a80ad733bac28da85e79da9c83c50
CamvuPham <camvupham@yahoo.com>
2016-12-01 17:25:16 -0800

    Add files via upload

---
+++ src/Controller.java
@@ -0,0 +1,44 @@
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+public class Controller {
+    
+    StoreModel model = new StoreModel();
+    
+    public void createUser(String username, String password, String email) {
+        model.createUser(username, password, email);
+    }
+    
+
+    public void editUser(String username, String password, String email, int isEmployee, int uID) {
+        model.editUser(username,  password,  email,  isEmployee,  uID);
+    }
+    
+
+    public void getUser(String username) {
+        model.getUser(username);
+    }
+    
+
+    public void makeOrder(int uID, int price) {
+        model.makeOrder( uID,  price);
+    }
+    
+
+    public void editOrder(int oID, int uID, int price) {
+        model.editOrder( oID,  uID,  price);
+    }
+
+
+    public void addFruit(String name, int price) {
+        model.addFruit( name,  price);
+    }
+    
+
+    public void getListOfFruit() {
+        model.getListOfFruit();
+    }
+    
+    // ...
+}
